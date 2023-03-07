ba08adc490528067b774b3f907f88cb122b965f5
berger <robertberger.dev@gmail.com>
2023-03-07 16:59:45 -0500

    created Home.js

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+import { Link, Outlet } from "react-router-dom"
+
+function Home() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/contacts" className="nav-link">Contacts</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Stack gap={3} className="col-md-10 mx-auto mt-3">
+        <Outlet />
+      </Stack>
+    </>
+  )
+}
+
+export default Home
