7d0b38376cd9a5140c5c48adca1a04a73f7104ee
berger <robertberger.dev@gmail.com>
2023-03-07 19:11:46 -0500

    setup LoadingIndicator

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Stack from 'react-bootstrap/Stack'
 import { Link, Outlet } from "react-router-dom"
+import LoadingIndicator from './LoadingIndicator'
 
 function Home() {
   return (
@@ -14,6 +15,9 @@ function Home() {
             <Link to="/contacts" className="nav-link">Contacts</Link>
             <Link to="/contacts/new" className="nav-link">New Contact</Link>
           </Nav>
+          <Navbar.Text>
+            <LoadingIndicator />
+          </Navbar.Text>
         </Container>
       </Navbar>
       <Stack gap={3} className="col-md-10 mx-auto mt-3">
